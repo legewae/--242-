8d42981206d7234d7dd234381c6bc402d3dc4c2d
legewae <55580266+legewae@users.noreply.github.com>
2025-06-05 11:53:49 +0600

    Add files via upload

---
+++ dynamic_array_test.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stddef.h>
+#include <assert.h>
+#include "linear_allocator.h"
+#include "dynamic_array.h"
+
+void test_create_array() {
+    linear_allocator* al = linear_allocator_init(1024);
+    ArrayList* list = malloc(sizeof(ArrayList));
+    arraylist_init(list, al);
+    assert(list->data == NULL && list->size == 0 && list->capacity == 0 && list->al == al);
+    linear_allocator_free(al);
+    free(list);
+}
+
+void test_add_element() {
+    linear_allocator* al = linear_allocator_init(1024);
+    ArrayList* list = malloc(sizeof(ArrayList));
+    arraylist_init(list, al);
+
+    int a = 42;
+    arraylist_add(list, &a, 0);
+    assert(list->size == 1 && list->capacity == 8 && *(int*)arraylist_get(list, 0) == 42);
+    linear_allocator_free(al);
+    free(list);
+}
+
+void test_add_element_at_index() {
+    linear_allocator* al = linear_allocator_init(1024);
+    ArrayList* list = malloc(sizeof(ArrayList));
+    arraylist_init(list, al);
+
+    int a = 42;
+    arraylist_add(list, &a, 0);
+
+    int b = 84;
+    arraylist_add(list, &b, 1);
+
+    assert(list->size == 2 && list->capacity == 8 && *(int*)arraylist_get(list, 0) == 42 && *(int*)arraylist_get(list, 1) == 84);
+    linear_allocator_free(al);
+    free(list);
+}
+
+void test_add_element_at_invalid_index() {
+    linear_allocator* al = linear_allocator_init(1024);
+    ArrayList* list = malloc(sizeof(ArrayList));
+    arraylist_init(list, al);
+
+    int a = 42;
+    arraylist_add(list, &a, 0);
+
+    int b = 84;
+    arraylist_add(list, &b, 2);
+
+    assert(list->size == 1 && list->capacity == 8 && *(int*)arraylist_get(list, 0) == 42);
+    linear_allocator_free(al);
+    free(list);
+}
+
+void test_add_element_exceeding_capacity() {
+    linear_allocator* al = linear_allocator_init(1024);
+    ArrayList* list = malloc(sizeof(ArrayList));
+    arraylist_init(list, al);
+
+    int values[10];
+    for (int i = 0; i < 10; i++) {
+        values[i] = i;
+        arraylist_add(list, &values[i], list->size);
+    }
+
+    assert(list->size == 10 && list->capacity == 16);
+    for (int i = 0; i < 10; i++) {
+        assert(*(int*)arraylist_get(list, i) == i);
+    }
+    linear_allocator_free(al);
+    free(list);
+}
+
+void test_del_middle() {
+    linear_allocator* al = linear_allocator_init(1024);
+    ArrayList* list = malloc(sizeof(ArrayList));
+    arraylist_init(list, al);
+
+    int a = 42;
+    arraylist_add(list, &a, 0);
+
+    int b = 84;
+    arraylist_add(list, &b, 1);
+
+    int c = 126;
+    arraylist_add(list, &c, 2);
+
+    arraylist_del(list, 1);
+    assert(list->size == 2 && list->capacity == 8 && *(int*)arraylist_get(list, 0) == 42 && *(int*)arraylist_get(list, 1) == 126);
+    linear_allocator_free(al);
+    free(list);
+}
+
+void test_add_middle() {
+    linear_allocator* al = linear_allocator_init(1024);
+    ArrayList* list = malloc(sizeof(ArrayList));
+    arraylist_init(list, al);
+
+    int a = 42;
+    arraylist_add(list, &a, 0);
+
+    int b = 84;
+    arraylist_add(list, &b, 1);
+
+    int c = 126;
+    arraylist_add(list, &c, 1);
+
+    assert(list->size == 3 && list->capacity == 8 &&
+        *(int*)arraylist_get(list, 0) == 42 &&
+        *(int*)arraylist_get(list, 1) == 126 &&
+        *(int*)arraylist_get(list, 2) == 84);
+    linear_allocator_free(al);
+    free(list);
+}
+
+int main() {
+    test_create_array();
+    test_add_element();
+    test_add_element_at_index();
+    test_add_element_at_invalid_index();
+    test_add_element_exceeding_capacity();
+    test_del_middle();
+    test_add_middle();
+    return 0;
+}
