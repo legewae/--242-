2da36e223b1a3afd055724b35517fb8d0d53d232
legewae <55580266+legewae@users.noreply.github.com>
2025-06-05 18:34:32 +0600

    Add files via upload

---
+++ hashtable.h
@@ -0,0 +1,23 @@
+#infdef HASHTABLE_H
+#define HASHTABLE_H
+
+typedef struct {
+	void* data;
+	char* key;
+	bucket* next;
+} bucket;
+
+typedef struct {
+	bucket** buckets;
+	pool_allocator* pl;
+	size_t capacity;
+} HashTable;
+
+
+int hashfunction(const char* key, size_t size);
+void hashtable_init(HashTable* ht, size_t capacity, pool_allocator* pl);
+void hashtable_insert(HashTable* ht, const char* key, void* value);
+void* hashtable_get(HashTable* ht, const char* key);
+void hashtable_del(HashTable* ht, const char* key);
+void hashtable_free(HashTable* ht);
+#endif
